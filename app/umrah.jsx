78bedc45a1d9caa49f5314aa26e3348d8b34a100
Nurzhan <nurzhan@Noutbuk-Nurzhan.local>
2024-07-28 14:35:21 +0500

    add: umrah

---
+++ app/umrah.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useRouter } from 'expo-router';
+
+
+
+const Umrah = () => {
+    const router = useRouter();
+    const navigation = useNavigation();
+    
+};
+
+
+export default Umrah;
